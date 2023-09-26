cc75b26af968f5be7efc74c9f07b4aaa44d5ed46
Henna <henna.hannila@gmail.com>
2023-09-26 18:07:03 +0300

    flatlist

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import DATA from './DATA';
+import { SafeAreaView } from 'react-native';
+import { FlatList } from 'react-native';
+import Row from './components/Row';
 
 export default function App() {
+
+  /*function renderRow({item}) {
+    return <Text>{item.lastname}</Text>
+  }*/
+
+ /* const renderRow = ({item}) => {
+        <Text>{item.lastname}</Text>
+  }*/
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+      data = {DATA}
+      renderItem={({item}) => (
+       <Row person={item} />
+      )}
+      keyExtractor={person => person.id}
+      >
+      </FlatList>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +33,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 40,
+    marginLeft: 20,
   },
 });
---
+++ DATA.js
@@ -0,0 +1,22 @@
+export default DATA = [
+    {
+        id: '1',
+        firstname: 'Henna',
+        lastname: 'Hannila'
+    },
+    {
+        id: '2',
+        firstname: 'Matti',
+        lastname: 'Meikäläinen'
+    },
+    {
+        id: '3',
+        firstname: 'Muumi',
+        lastname: 'Peikko'
+    },
+    {
+        id: '4',
+        firstname: 'Pikku',
+        lastname: 'Myy'
+    },
+]
